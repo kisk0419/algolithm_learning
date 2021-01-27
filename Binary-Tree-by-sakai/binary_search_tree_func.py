e3054808491eb0962705bf3a2bedc6a9d9ef441e
Keisuke Baba <kisk0419@gmail.com>
2021-01-27 10:55:10 +0900

    Add inorder and search funcs

--- Binary-Tree-by-sakai/binary_search_tree_func.py
+++ Binary-Tree-by-sakai/binary_search_tree_func.py
@@ -17,12 +17,32 @@ def insert(node: Node, value: int) -> Node:
     return node
 
 
+def inorder(node: Node) -> None:
+    if not node:
+        return
+    inorder(node.left)
+    print(node.value)
+    inorder(node.right)
+
+
+def search(node: Node, value: int) -> bool:
+    if node is None:
+        return False
+    
+    if node.value == value:
+        return True
+    elif value < node.value:
+        return search(node.left, value)
+    else:
+        return search(node.right, value)
+
+
+
 if __name__ == '__main__':
     root = None
     datas = [3, 6, 5, 7, 1, 10, 2]
     for data in datas:
         root = insert(root, data)
 
-    print(root.value)
-    print(root.left.right.value)
-    print(root.right.left.value)
+    inorder(root)
+    print(search(root, 11))
