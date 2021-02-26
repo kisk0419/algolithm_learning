475529a845eefe3e85ad49233d57b9b8065be44a
Keisuke Baba <kisk0419@gmail.com>
2021-02-26 10:13:25 +0900

    Add hanoi quiz

---
+++ Coding-Quiz/hanoi.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+
+def hanoi(disk: int, src: str, dest: str, support: str) -> None:
+    if disk < 1:
+        return
+    hanoi(disk - 1, src, support, dest)
+    print(f'move {disk} {src} to {dest}')
+    hanoi(disk - 1, support, dest, src)
+
+
+def get_hanoi_movement(disk: int, src: str, dest: str, support: str) -> List[Tuple[int, str, str]]:
+    result = []
+
+    def _hanoi(disk: int, src: str, dest: str, support: str) -> None:
+        if disk < 1:
+            return
+        _hanoi(disk - 1, src, support, dest)
+        result.append((disk, src, dest))
+        _hanoi(disk - 1, support, dest, src)
+    _hanoi(disk, src, dest, support)
+    return result
+
+if __name__ == '__main__':
+    hanoi(3, 'A', 'C', 'B')
+    for r in get_hanoi_movement(3, 'A', 'C', 'B'):
+        print(r)
